4769c27065dd1894278b7d1aaaaa1446e30731e5
evgeniavolkova <evgeniafil04@gmail.com>
2025-05-29 11:53:28 +0500

    fix babka

---
+++ Assets/scprits/EnemyScripts/MusicTriggerZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MusicTriggerZone : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Stop();
+        }
+    }
+}
